9e7424f606efe22ae176cd92d5bbbabb6a0ad56b
MichalPham <phamstm@gmail.com>
2019-12-09 20:06:49 +0100

    pwa

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <ion-grid>
+        <form>
+            <ion-col>
+                <ion-item>
+                    <ion-label>
+                        Zadani:
+                    </ion-label>
+                    <ion-input name="input"></ion-input>
+                </ion-item>
+            </ion-col>
+            <ion-col>
+                <ion-button type="submit" color="primary" expand="block">Lustit</ion-button>
+            </ion-col>
+        </form>
+    </ion-grid>
+</template>
+
+<script>
+    export default {
+        name: "input.component"
+    }
+</script>
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "MenuComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
